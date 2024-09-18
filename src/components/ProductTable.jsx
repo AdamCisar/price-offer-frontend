50c76d14681769e126d22d61256c2dd7d6786087
Adam Čisár <adamcisar7@gmail.com>
2024-09-18 21:39:46 +0200

    mui instalacia

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,27 +1,31 @@
 import { ProductsContext } from "../providers/ProductsProvider";
 import { useContext } from "react";
 import ProductSnapshot from "./ProductSnapshot";
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Paper } from '@mui/material';
+
 
 const ProductTable = () => {
     const { products, isLoading, error } = useContext(ProductsContext);
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Nazov</th>
-                    <th>Cena</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {products && products.map((product) => (
-                    <tr key={product.id}>
-                        <ProductSnapshot {...product} />
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <TableContainer component={Paper} sx={{ maxWidth: 850 }} >
+        <Table aria-label="simple table" >
+          <TableHead>
+            <TableRow>
+              <TableCell>Nazov</TableCell>
+              <TableCell>Cena</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products && products.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{product.price}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
 }
 
