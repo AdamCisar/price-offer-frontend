286e7f0d6cc33fde6926fdb709a3c850bb04aeb4
Adam Čisár <adamcisar7@gmail.com>
2024-10-18 19:59:57 +0200

    zmena prefix api

--- src/configuration/api_routes/ApiRoutes.jsx
+++ src/configuration/api_routes/ApiRoutes.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_BASE_URL + '/v1/api';
+const API_BASE_URL = process.env.REACT_APP_BASE_URL + '/backend/api';
 export const IMAGES_FOLDER = process.env.REACT_APP_BASE_URL + '/images';
 
 const ApiRoutes = {
